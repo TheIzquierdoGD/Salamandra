2c599d22bb45d05e5be92435573179aa4de993b2
TheIzquierdoGD <giovannyizquierdoaranguri@gmail.com>
2026-03-05 19:54:24 -0500

    Update AntiSwitcher.cpp

--- src/all/AntiSwitcher.cpp
+++ src/all/AntiSwitcher.cpp
@@ -1,21 +1,15 @@
-#include <geode.core.hpp>
-#include <geode.utils/web.hpp>
+#include <Geode/Geode.hpp>
+#include <Geode/utils/web.hpp>
 
 using namespace geode::prelude;
 
 void testAuth() {
-    // Su clave (debe coincidir con $SECRET en auth.php)
     std::string token = "EJEMPLODESEGURIDAD";
 
     auto req = web::WebRequest();
-
-    // Agregar header Authorization
     req.header("Authorization", token);
 
-    // Hacer petición GET
-    auto result = req.get("https://salamandra.ps.fhgdps.com/incl/auth.php");
-
-    result
+    req.get("https://salamandra.ps.fhgdps.com/incl/auth.php")
         .then([](web::WebResponse* response) {
             log::info("Respuesta del servidor: {}", response->string());
         })
