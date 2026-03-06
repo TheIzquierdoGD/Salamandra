4e37444b6bd41ac9c960394e2e21a3d0f2dedf1e
TheIzquierdoGD <giovannyizquierdoaranguri@gmail.com>
2026-03-05 19:57:46 -0500

    Update AntiSwitcher.cpp

--- src/all/AntiSwitcher.cpp
+++ src/all/AntiSwitcher.cpp
@@ -6,14 +6,18 @@ using namespace geode::prelude;
 void testAuth() {
     std::string token = "EJEMPLODESEGURIDAD";
 
-    auto req = web::WebRequest();
+    web::WebRequest req;
     req.header("Authorization", token);
 
-    req.get("https://salamandra.ps.fhgdps.com/incl/auth.php")
-        .then([](web::WebResponse* response) {
-            log::info("Respuesta del servidor: {}", response->string());
-        })
-        .except([](std::string const& error) {
-            log::error("Error en request: {}", error);
-        });
+    auto task = req.get("https://salamandra.ps.fhgdps.com/incl/auth.php");
+
+    task.wait(); // Espera la respuesta (forma simple)
+
+    if (task.isResolved()) {
+        auto response = task.get();
+        log::info("Respuesta del servidor: {}", response.string());
+    }
+    else {
+        log::error("Error en la petición");
+    }
 }
